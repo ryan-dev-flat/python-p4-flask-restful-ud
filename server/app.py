f4a0a609e5daa2869f29f72f6296885d9de3b86d
Ryan <coap.ryanmontgomery@gmail.com>
2024-07-23 20:51:12 -0600

    done

--- server/app.py
+++ server/app.py
@@ -19,11 +19,11 @@ api = Api(app)
 class Home(Resource):
 
     def get(self):
-        
+
         response_dict = {
             "message": "Welcome to the Newsletter RESTful API",
         }
-        
+
         response = make_response(
             response_dict,
             200,
@@ -36,7 +36,7 @@ api.add_resource(Home, '/')
 class Newsletters(Resource):
 
     def get(self):
-        
+
         response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
 
         response = make_response(
@@ -47,7 +47,7 @@ class Newsletters(Resource):
         return response
 
     def post(self):
-        
+
         new_record = Newsletter(
             title=request.form['title'],
             body=request.form['body'],
@@ -76,6 +76,32 @@ class NewsletterByID(Resource):
         response = make_response(
             response_dict,
             200,
+        )
+
+        return response
+
+    def patch(self, id):
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+        response = make_response(response_dict, 200)
+        return response
+    def delete(self, id):
+
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
         )
 
         return response
